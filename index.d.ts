2a35c2f917ae119b2aada18237ca4d5fa0c34b5e
Nathan Jhaveri <nathanj@porch.com>
2021-05-27 13:41:31 -0700

    Typescript types for it.nock
    Types are needed to inform tsc that the jest `it` function has `it.nock`

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import nock from "nock/types";
+
+type FnFn = () => () => void;
+type NockTest = (title: string, fn: FnFn, timeout?: number) => void;
+
+declare global {
+    namespace jest {
+        interface It {
+            nock: NockTest;
+        }
+    }
+}
+
+type Options = {
+    writeAfterEach?: boolean;
+    loadAfterEach?: boolean;
+    nockOptions?: nock.Options;
+};
+
+export function upgradeJasmine(global: NodeJS.Global, options?: Options): void;
